ab4f698032868bb21ae0cb49502b52201a2a6d89
Rishabh <mailrishabh1403@gmail.com>
2017-08-31 15:37:52 +0530

    new files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDf86SrLkWSvkdqY5FY4splzRO8C5hSmtw';
+
+const App = () => {
+    return <div> Hi </div>;
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
