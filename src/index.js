c9dbdca5bb0b7ed095b98a7ac7b1e960b84fb394
Rishabh <mailrishabh1403@gmail.com>
2017-09-01 21:30:16 +0530

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDf86SrLkWSvkdqY5FY4splzRO8C5hSmtw';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            videos : []
+        };
+        YTSearch({ key: API_KEY, term: 'jain' }, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
+
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
