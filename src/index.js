8423d8e3322a69a5269b29f20001edbf769ba20b
Rishabh <mailrishabh1403@gmail.com>
2017-09-02 13:39:43 +0530

    complete needs styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 import './style.css';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDf86SrLkWSvkdqY5FY4splzRO8C5hSmtw';
 
@@ -30,13 +31,11 @@ class App extends React.Component {
     handleVideoSelect(selected){
         this.setState({selected});
     }
-    handleInputChange(term){
-        this.searchVideos(term);
-    }
     render() {
+        const vidSearch = _.debounce(term => {this.searchVideos(term)} , 1000);
         return (
             <div>
-                <SearchBar onInputChange={this.handleInputChange.bind(this)} />
+                <SearchBar onInputChange={vidSearch.bind(this)} />
                 {this.state.selected && <VideoDetail video={this.state.selected}/>}
                 <VideoList onVideoSelect={this.handleVideoSelect.bind(this)} videos={this.state.videos}/>
             </div>
